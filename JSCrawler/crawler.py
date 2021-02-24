556a1d0b123b69202b17dbf4c952c86c03a28c4e
Komi <1186439457@qq.com>
2021-02-24 15:19:09 +0800

    Create crawler.py
    A crawler that collects JS files on your target website.

---
+++ JSCrawler/crawler.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python"
+# coding: utf-8
+# By Threezh1
+# https://threezh1.github.io/
+# Modify by CFHB
+# A crawler that collects JS files on your target website
+
+import requests, argparse, sys, re
+from requests.packages import urllib3
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import hashlib,base64
+
+def parse_args():
+    parser = argparse.ArgumentParser(epilog='\tExample: \r\npython ' + sys.argv[0] + " -u http://www.baidu.com")
+    parser.add_argument("-u", "--url", help="The website")
+    parser.add_argument("-c", "--cookie", help="The website cookie")
+
+    return parser.parse_args()
+
+# Regular expression comes from https://github.com/GerbenJavado/LinkFinder
+def extract_URL(JS):
+	pattern_raw = r"""
+	  (?:"|')                               # Start newline delimiter
+	  (
+	    ((?:[a-zA-Z]{1,10}://|//)           # Match a scheme [a-Z]*1-10 or //
+	    [^"'/]{1,}\.                        # Match a domainname (any character + dot)
+	    [a-zA-Z]{2,}[^"']{0,})              # The domainextension and/or path
+	    |
+	    ((?:/|\.\./|\./)                    # Start with /,../,./
+	    [^"'><,;| *()(%%$^/\\\[\]]          # Next character can't be...
+	    [^"'><,;|()]{1,})                   # Rest of the characters can't be
+	    |
+	    ([a-zA-Z0-9_\-/]{1,}/               # Relative endpoint with /
+	    [a-zA-Z0-9_\-/]{1,}                 # Resource name
+	    \.(?:[a-zA-Z]{1,4}|action)          # Rest + extension (length 1-4 or action)
+	    (?:[\?|/][^"|']{0,}|))              # ? mark with parameters
+	    |
+	    ([a-zA-Z0-9_\-]{1,}                 # filename
+	    \.(?:php|asp|aspx|jsp|json|
+	         action|html|js|txt|xml)             # . + extension
+	    (?:\?[^"|']{0,}|))                  # ? mark with parameters
+	  )
+	  (?:"|')                               # End newline delimiter
+	"""
+	pattern = re.compile(pattern_raw, re.VERBOSE)
+	result = re.finditer(pattern, str(JS))
+	if result == None:
+		return None
+	js_url = []
+	return [match.group().strip('"').strip("'") for match in result
+		if match.group() not in js_url]
+
+# Get the page source
+def Extract_html(URL):
+	header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36",
+	"Cookie": args.cookie}
+	try:
+		raw = requests.get(URL, headers = header, timeout=3, verify=False)
+		raw = raw.content.decode("utf-8", "ignore")
+
+		return raw
+	except Exception as e:
+		print(e)
+		return None
+
+# Handling relative URLs
+def process_url(URL, re_URL):
+	black_url = ["javascript:"]	# Add some keyword for filter url.
+	URL_raw = urlparse(URL)
+	ab_URL = URL_raw.netloc
+	host_URL = URL_raw.scheme
+	if re_URL[0:2] == "//":
+		result = host_URL  + ":" + re_URL
+	elif re_URL[0:4] == "http":
+		result = re_URL
+	elif re_URL[0:2] != "//" and re_URL not in black_url:
+		if re_URL[0:1] == "/":
+			result = host_URL + "://" + ab_URL + re_URL
+		else:
+			if re_URL[0:1] == ".":
+				if re_URL[0:2] == "..":
+					result = host_URL + "://" + ab_URL + re_URL[2:]
+				else:
+					result = host_URL + "://" + ab_URL + re_URL[1:]
+			else:
+				result = host_URL + "://" + ab_URL + "/" + re_URL
+	else:
+		result = URL
+	return result
+
+def find_last(string,str):
+	positions = []
+	last_position=-1
+	while True:
+		position = string.find(str,last_position+1)
+		if position == -1:break
+		last_position = position
+		positions.append(position)
+	return positions
+
+def save_jsfile(js_link):
+	try:
+
+		# link 进行 md5加密，用于拼接在js文件名称里面，用于防止文件覆盖的比如： www.baidu.com/oa/index.js 和 www.baidu.com/web/index.js
+		m = hashlib.md5(js_link.encode())
+		res = m.hexdigest()
+
+		js_text = Extract_html(js_link)
+		if "</html>" in js_text or "<html>" in js_text:
+			return
+		url_raw = urlparse(js_link)
+		path = url_raw.path
+		jsFilename = res+"_"+path[path.rindex("/") + 1:]
+		# print(js_link+"===>"+jsFilename)
+		with open("/Users/80252519/APPTest/FridaTest/ziwu-javascript-rules/testcases/"+jsFilename, "w+") as wf:
+			wf.write(js_text)
+	except:
+		return
+
+def find_by_url(url, js = False):
+	if js == False:
+		try:
+			print("url:" + url)
+		except:
+			print("Please specify a URL like https://www.baidu.com")
+		html_raw = Extract_html(url)
+		if html_raw == None:
+			print("Fail to access " + url)
+			return None
+		#print(html_raw)
+		html = BeautifulSoup(html_raw, "html.parser")
+		html_scripts = html.findAll("script")
+		script_array = {}
+		script_temp = ""
+		for html_script in html_scripts:
+			script_src = html_script.get("src")
+			if script_src == None:
+				script_temp += html_script.get_text() + "\n"
+			else:
+				purl = process_url(url, script_src)
+				save_jsfile(purl)
+				script_array[purl] = Extract_html(purl)
+		script_array[url] = script_temp
+		allurls = []
+		for script in script_array:
+			temp_urls = extract_URL(script_array[script])
+			if len(temp_urls) == 0: continue
+			for temp_url in temp_urls:
+				allurls.append(process_url(script, temp_url))
+		result = []
+		for singerurl in allurls:
+			url_raw = urlparse(url)
+			domain = url_raw.netloc
+			positions = find_last(domain, ".")
+			miandomain = domain
+			if len(positions) > 1:miandomain = domain[positions[-2] + 1:]
+			#print(miandomain)
+			suburl = urlparse(singerurl)
+			subdomain = suburl.netloc
+			#print(singerurl)
+			if miandomain in subdomain or subdomain.strip() == "":
+				if singerurl.strip() not in result:
+					result.append(singerurl)
+		return result
+	return sorted(set(extract_URL(Extract_html(url)))) or None
+
+def find_by_url_deep(url):
+	html_raw = Extract_html(url)
+	# print(html_raw)
+	if html_raw == None: 
+		print("Fail to access " + url)
+		return None
+	html = BeautifulSoup(html_raw, "html.parser")
+	html_as = html.findAll("a")
+	links = []
+	for html_a in html_as:
+		src = html_a.get("href")
+		# print(src)
+		if src == "" or src == None: continue
+		link = process_url(url, src)
+		if link not in links:
+			links.append(link)
+	if links == []:
+		links = find_by_url(url)
+
+		return
+
+	print("ALL Find " + str(len(links)) + " links")
+	urls = []
+	i = len(links)
+	for link in links:
+		temp_urls = find_by_url(link)
+		if temp_urls == None: continue
+		print("Remaining " + str(i) + " | Find " + str(len(temp_urls)) + " URL in " + link)
+		for temp_url in temp_urls:
+			if temp_url not in urls:
+				urls.append(temp_url)
+		i -= 1
+	return urls
+
+if __name__ == "__main__":
+	urllib3.disable_warnings()
+	args = parse_args()
+	urls = find_by_url_deep(args.url)
